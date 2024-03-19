6412e819d6282b959de77dc59141ae98a44aa9b2
Maria Pasca <maria13pasca@gmail.com>
2024-03-19 18:49:29 +0200

    updated search location

--- daweb_30644_pasca_maria/src/pages/SearchLocation.js
+++ daweb_30644_pasca_maria/src/pages/SearchLocation.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SearchLocation(){
     const[location, setLocation] = useState();
 
+    var existingLocations = JSON.parse(localStorage.getItem("mocks"));
+    existingLocations = existingLocations.map((mock) => mock.location);
+    console.log(existingLocations);
     const handleClick = () => {
         localStorage.setItem("chosenLocation", location);
     }
@@ -15,9 +18,12 @@ export default function SearchLocation(){
                 <hr class="titleLine" style={{marginBottom:"-5%"}}/>
                 <div style={{width:"80%", marginTop: "5%"}}>
                     <h2 class="h2Title">Choose a location</h2>
-                    <input class="btnTitle" placeholder="Type here" style={{marginBottom:"10%", width:"200px", height:"30px"}}
-                        id="txtLocation" value={location} onChange={(e) => setLocation(e.target.value)}/>
-                    <Link to="/search_date"><button class="btnNav" style={{ display:"flow", marginLeft:"75%", marginTop:"-5%"}} onClick={handleClick}>Next</button></Link>
+                    <select class="btnTitle"
+                        value={existingLocations[0].location}
+                        onChange={e => setLocation(e.target.value)}
+                        >{existingLocations.map((mock) => <option value={mock}>{mock}</option>)}
+                    </select>
+                    <Link to="/search_date"><button class="btnNav" style={{ display:"flow", marginLeft:"75%", marginTop:"5%"}} onClick={handleClick}>Next</button></Link>
                     <Link to="/home"><button class="btnNav" style={{ display:"flow", marginLeft:"0%", marginTop:"-5%"}}>Back</button></Link>
                 </div>
             </div>
