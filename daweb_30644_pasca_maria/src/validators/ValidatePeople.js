a993ca2683ce11447f1871a4735d5043047ca5d1
Maria Pasca <maria13pasca@gmail.com>
2024-03-21 12:38:03 +0200

    people validator

---
+++ daweb_30644_pasca_maria/src/validators/ValidatePeople.js
@@ -0,0 +1,26 @@
+export default function ValidatePeople (noAdults, noChildren){
+    if(noAdults < 0 || noChildren < 0){
+        console.log("number of adults: " + noAdults);
+        console.log("number of children: " + noChildren);
+        throw Error("Number of goers cannot be negative");
+    }
+    if(noAdults === 0 && noChildren !== 0){
+        console.log("number of adults: " + noAdults);
+        console.log("number of children: " + noChildren);
+        throw Error("No number of adults specified")
+    }
+    var sum = parseInt(noAdults) + parseInt(noChildren);
+    console.log(sum);
+    if(sum > 8){
+        console.log("number of adults: " + noAdults);
+        console.log("number of children: " + noChildren);
+        throw Error("Too many people");
+    }
+    if(noChildren/noAdults > 3){
+        console.log("number of adults: " + noAdults);
+        console.log("number of children: " + noChildren);
+        throw Error("An adult should look over maximum 3 children");
+    }
+
+    return true;
+}
